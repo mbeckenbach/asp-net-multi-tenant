a9de4c364cc4574dcaf27e83c560a0ecf9eabe73
@max126 <max@rocketloop.de>
2020-01-22 10:40:09 +0100

    Basic Web Api

--- StarFleetOs/Startup.cs
+++ StarFleetOs/Startup.cs
@@ -16,24 +16,28 @@ namespace StarFleetOs
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Provide MVC
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Error Handling
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            // Adds endpoint routing support
             app.UseRouting();
 
+            // Sets default endpoint routes
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
             });
         }
     }
